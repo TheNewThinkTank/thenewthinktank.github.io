0c8dbda9975ca6fb56c26f27ed22a07c8d657024
Gustav Rasmussen <TheNewThinkTank@users.noreply.github.com>
2025-05-24 15:59:55 +0200

    fix: Wrap js code in a DOMContentLoaded event to fix dark mode toggle on profile page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 
-const toggle = document.getElementById('dark-mode-toggle');
-
-toggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
+document.addEventListener('DOMContentLoaded', function () {
+    const toggle = document.getElementById('dark-mode-toggle');
+    if (toggle) {
+        toggle.addEventListener('click', () => {
+            document.body.classList.toggle('dark-mode');
+        });
+    }
 });
